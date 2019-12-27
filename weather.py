a4e1ca35a9012c42be11c1c506cb806fc84153f6
Luis Correa <correaleyval@gmail.com>
2019-12-27 14:18:39 -0500

    Fixed by @pavelmc

--- weather.py
+++ weather.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env python3
-
-from urllib import request
+import urllib.request
+import urllib.parse
 import json
 
 API = 'https://www.redcuba.cu/api/weather_get_summary/{location}'
 
 class RCApiClient:
+    
     def __init__(self, province):
-        response = request.urlopen(API.format(location=province))
+        escapedProv = urllib.parse.quote_plus(province)
+        url = API.format(location=escapedProv)
+        print(url)
+        response = urllib.request.urlopen(url)
         self.data = json.loads(response.read())
 
     def getTemperature(self):
         return self.data['data']['temp']
+
 
     def getHumidity(self):
         return self.data['data']['humidity']
@@ -23,7 +27,7 @@ class RCApiClient:
         return self.data['data']['descriptionWeather']
 
 def main():
-    c = RCApiClient("Provincia%20de%20Cienfuegos")
+    c = RCApiClient("Cienfuegos")
     print(c.getTemperature())
 
 if __name__ == '__main__':
