2de1e2090f47279356677b3388268e3bd0d9e77b
Gabriel A. López López <glpz@daxslab.com>
2019-12-27 13:16:24 -0500

    hello weather

---
+++ weather.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import urllib
+import json
+
+API = 'https://www.redcuba.cu/api/weather_get_summary/{location}'
+
+class RCApiClient:
+    
+    def __init__(self, province):
+        response = urllib.request.urlopen(API.format(location=province))
+        self.data = json.loads(reponse.read())
+
+    def getTemperature(self):
+        return self.data['data']['temp']
+
+
+    def getHumidity(self):
+        return self.data['data']['humidity']
+
+    def getPressure(self):
+        return self.data['data']['pressure']
+
+    def getGeneral(self):
+        return self.data['data']['descriptionWeather']
+
+def main():
+    c = RCApiClient("Provincia de Cienfuegos")
+    print(RCApiClient.getTemperature())
+
+if __name__ == '__main__':
+    main()
