5757ecfdcafc2d411b9cbe6afe5f742b732d06cf
Gabriel A. López López <glpz@daxslab.com>
2019-12-27 14:06:25 -0500

    fixed the request petition

--- weather.py
+++ weather.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-from urllib import request
+import urllib.request
+import urllib.parse
 import json
 
 API = 'https://www.redcuba.cu/api/weather_get_summary/{location}'
 
 class RCApiClient:
     def __init__(self, province):
-        response = request.urlopen(API.format(location=province))
-        self.data = json.loads(response.read())
+        escapedProv = urllib.parse.quote_plus(province)
+        url = API.format(location=escapedProv)
+        response = urllib.request.urlopen(url)
+        self.data = json.loads(response.read()) 
 
     def getTemperature(self):
         return self.data['data']['temp']
@@ -23,7 +26,7 @@ class RCApiClient:
         return self.data['data']['descriptionWeather']
 
 def main():
-    c = RCApiClient("Provincia%20de%20Cienfuegos")
+    c = RCApiClient("Cienfuegos")
     print(c.getTemperature())
 
 if __name__ == '__main__':
