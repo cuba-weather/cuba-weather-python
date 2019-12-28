9d9b745ca5ce21faf5c093b5f7c3ee996661ee9b
Sandy Fernando Perez Matamoros <sandypm007@gmail.com>
2019-12-27 20:09:34 -0500

    Agregando control de errores HTTP no entontrado 404

--- weather.py
+++ weather.py
@@ -4,20 +4,26 @@ import json
 import urllib.parse
 import urllib.request
 from sys import argv
+from urllib.error import HTTPError
 
 API = 'https://www.redcuba.cu/api/weather_get_summary/{location}'
 
 
 class RCApiClient:
+    data = False
 
     def __init__(self, province):
         escaped_prov = urllib.parse.quote(province)
         url = API.format(location=escaped_prov)
-        response = urllib.request.urlopen(url)
-        content = response.read()
-        if type(content) == bytes:
-            content = content.decode()
-        self.data = json.loads(content)
+        try:
+            response = urllib.request.urlopen(url)
+            content = response.read()
+            if type(content) == bytes:
+                content = content.decode()
+            self.data = json.loads(content)
+        except HTTPError as ex:
+            print('No se ha encontrado la ciudad' if ex.code == 404 else ex)
+            exit(-1)
 
     def getTemperature(self):
         return self.data['data']['temp']
