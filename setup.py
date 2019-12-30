83fd589fef3eee9b4e663bf5fdfd7aa72feaa225
Luis Correa <correaleyval@gmail.com>
2019-12-30 11:02:07 -0500

    Settings for PyPi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cuba_weather-correaleyval", # Replace with your own username
+    version="0.0.1",
+    author="Cuban OpenSource",
+    author_email="correaleyval@gmail.com",
+    description="Python3 client for (https://www.redcuba.cu) weather API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/correaleyval/cuba-weather",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
