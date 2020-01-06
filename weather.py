118cff031df002b48d05d13a75fb0d84fe260ab9
Gabriel A. López López <glpz@daxslab.com>
2020-01-06 01:18:09 -0500

    add general argument

--- weather.py
+++ weather.py
@@ -12,9 +12,9 @@ API = 'https://www.redcuba.cu/api/weather_get_summary/{location}'
 class RCApiClient:
     data = False
 
-    def __init__(self, province):
-        escaped_prov = urllib.parse.quote(province)
-        url = API.format(location=escaped_prov)
+    def __init__(self,location):
+        escaped_location = urllib.parse.quote(location)
+        url = API.format(location=escaped_location)
         try:
             response = urllib.request.urlopen(url)
             content = response.read()
@@ -22,7 +22,7 @@ class RCApiClient:
                 content = content.decode()
             self.data = json.loads(content)
         except HTTPError as ex:
-            raise Exception('No se ha encontrado la ciudad' if ex.code == 404 else ex)
+            raise Exception('Location not found' if ex.code == 404 else ex)
 
     def getTemperature(self):
         return self.data['data']['temp']
@@ -39,23 +39,25 @@ class RCApiClient:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('province', type=str, help='Province name')
+    parser.add_argument('location', type=str, help='Location name')
     parser.add_argument("-v", "--version", help="Shows program version", action='store_true')
-    parser.add_argument("-t", "--temperature", help="Shows province temperature", action='store_true')
-    parser.add_argument("-u", "--humidity", help="Shows province humidity", action='store_true')
-    parser.add_argument("-p", "--pressure", help="Shows province pressure", action='store_true')
+    parser.add_argument("-t", "--temperature", help="Shows location temperature", action='store_true')
+    parser.add_argument("-u", "--humidity", help="Shows location humidity", action='store_true')
+    parser.add_argument("-p", "--pressure", help="Shows location pressure", action='store_true')
+    parser.add_argument("-g", "--general", help="Shows location general information", action='store_true')
 
     args = parser.parse_args()
-    c = RCApiClient(args.province)
+    c = RCApiClient(args.location)
     if args.version:
         print("this is myprogram version 0.1")
+    if args.general:
+        print(c.getGeneral())
     if args.temperature:
-        print("Temperatura: {temp}°C".format(temp=c.getTemperature()))
+        print("Temperature: {temp}°C".format(temp=c.getTemperature()))
     if args.humidity:
-        print("Humedad: {hum}%".format(hum=c.getHumidity()))
+        print("Humidity: {hum}%".format(hum=c.getHumidity()))
     if args.pressure:
-        print("Presión atmosférica: {hpa} hpa".format(hpa=c.getPressure()))
-
+        print("Pressure: {hpa} hpa".format(hpa=c.getPressure()))
 
 if __name__ == '__main__':
     main()
