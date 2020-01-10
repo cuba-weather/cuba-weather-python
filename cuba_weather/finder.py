d230dabf52db41eac3cb46d68dfef9668fb08854
leynier <leynier41@gmail.com>
2020-01-10 10:35:54 -0500

    Implement finder with Levenshtein distance

--- cuba_weather/finder.py
+++ cuba_weather/finder.py
@@ -1,3 +1,4 @@
+from Levenshtein import distance
 from locations import locations
 
 locations = sorted(locations, key=len)
@@ -8,4 +9,12 @@ def get_location(user_input: str) -> str:
     for location in locations:
         if lower_input in location.lower():
             return location
-    return user_input
+    best_location = locations[0].lower()
+    best_distance = distance(lower_input, best_location)
+    for i in range(1, len(locations)):
+        actual_location = locations[i]
+        actual_distance = distance(user_input, actual_location)
+        if actual_distance < best_distance:
+            best_location = actual_location
+            best_distance = actual_distance
+    return best_location
