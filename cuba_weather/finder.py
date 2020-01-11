868b08c03a0cc01e747cdc3b65856047b67ccc41
leynier <leynier41@gmail.com>
2020-01-11 02:38:14 -0500

    Implement Damerau Levenshtein distance without external libraries

--- cuba_weather/finder.py
+++ cuba_weather/finder.py
@@ -1,7 +1,24 @@
-from Levenshtein import distance
 from locations import locations
 
 locations = sorted(locations, key=len)
+
+
+def distance(s1, s2):
+    d = {}
+    for i in range(-1, len(s1) + 1):
+        d[i, -1] = i + 1
+    for j in range(-1, len(s2) + 1):
+        d[-1, j] = j + 1
+    for i, cs1 in enumerate(s1):
+        for j, cs2 in enumerate(s2):
+            cost = int(cs1 != cs2)
+            d[i, j] = min(d[i - 1, j] + 1, d[i, j - 1] + 1, d[i - 1, j - 1] + cost)
+            if not i or not j:
+                continue
+            if cs1 != cs2:
+                continue
+            d[i, j] = min(d[i, j], d[i - 2, j - 2] + cost)
+    return d[len(s1) - 1, len(s2) - 1]
 
 
 def get_location(user_input: str) -> str:
